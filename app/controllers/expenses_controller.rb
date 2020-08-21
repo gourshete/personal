dac7fc90eba9654cc81071c621668f959b72e3f3
gourshete <swapnilggourshete@gmail.com>
2020-08-22 00:15:04 +0530

    - add provision to securely access keys - update styling for login & sign up page

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -4,4 +4,42 @@ class ExpensesController < ApplicationController
     data = Expense.limit(10)
     render json: { data: data, status: :ok }
   end
+
+  def create
+    render json: { status: :ok , message: 'Record created successfully' }
+  rescue StandardError => e
+    render json: { status: 400, message: e.message }
+  end
+
+  def update
+    render json: { status: :ok , message: 'Record created successfully' }
+  rescue StandardError => e
+    render json: { status: 400, message: e.message }
+  end
+
+
+  private
+  def expense_params
+    params.permit(:description, :amount, :category_id, :expense_id)
+  end
+
+  def create_expense
+    create_params = {
+        description: expense_params[:description],
+        amount: expense_params[:amount],
+        category_id: expense_params[:category_id],
+        user_id: current_user.id
+    }
+    Expense.create!(create_params)
+  end
+
+  def update_expense
+    update_params = {
+        description: expense_params[:description],
+        amount: expense_params[:amount],
+        category_id: expense_params[:category_id],
+    }
+    expense = Expense.find_by(id: expense_params[:id])
+    expense.update!(update_params)
+  end
 end
